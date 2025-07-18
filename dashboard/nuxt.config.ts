b106099888dc3c1fe004d8010872ed12fffed1da
iMatheusPaula <imatheuspaula@gmail.com>
2025-07-18 17:20:01 -0300

    feat: upgrading to nuxt 4 and applying new folder structure

--- dashboard/nuxt.config.ts
+++ dashboard/nuxt.config.ts
@@ -1,35 +1,36 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  components: true,
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
     runtimeConfig: {
-      public: {
-          API_URL: process.env.API_URL,
-      }
+        public: {
+            API_URL: process.env.API_URL,
+        }
     },
-  modules: [
-      '@pinia/nuxt',
-      '@pinia-plugin-persistedstate/nuxt',
-      '@nuxt/test-utils/module'
-  ],
-  css: [
-      '~/assets/css/main.css',
-      '~/assets/css/fonts.css',
-      '~/assets/css/base.css'
-  ],
-  app:{
-    head: {
-      link: [
-          { rel:"stylesheet", type:"text/css",
-            href:'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
-          }
-      ]
-    }
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    modules: [
+        '@pinia/nuxt',
+        '@nuxt/test-utils/module'
+    ],
+    css: [
+        '~/assets/css/main.css',
+        '~/assets/css/fonts.css',
+        '~/assets/css/base.css'
+    ],
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet", type: "text/css",
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+                }
+            ]
+        }
     },
-  },
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
 })
